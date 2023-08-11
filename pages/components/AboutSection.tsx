1d39c16f185893991c01d0cad55d8b00c06d9998
Abderrahim Zabir <bahimzabir@gmail.com>
2023-08-11 10:04:57 +0100

    finishing

---
+++ pages/components/AboutSection.tsx
@@ -0,0 +1,84 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "Python" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+  { skill: "Jupyter Notebooks" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is 
+              <span className="font-bold">{" Abderrahim Zabir"}</span>, and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, tech explorer dedicated to making 
+            the world of technology more exciting.
+            </p>
+            <br />
+            <p>
+            I'm proud to be part of 
+            <a href="https://www.42network.org/about-us/" target="_blank" className="font-bold text-teal-500"> 42 Network </a> as a sudent at 
+            <a href="https://1337.ma/" target="_blank" className="font-bold text-teal-500"> 1337 </a>
+            coding school in Morocco.
+            and holding a degree in renewable energies and technologies from Ibnzohr University, I bring a unique blend of skills to the table.
+            </p>
+            <br />
+            <p>
+              My real passion lies in crafting innovative software that challenges the status quo.
+              Whether it's creating user-friendly apps or building robust backends, 
+              I thrive on turning ideas into impactful solutions. I'm also a devoted tech geek,
+              and always eager to dive into new technologies. This drive pushes me to create software
+              that not only keeps up with the times, but also shapes the future.
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image
+              src="/dev-ed-wave.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,18 @@
 "use client"
 import Navbar from "./components/navbar"
-import {ThemeProvider} from 'next-themes'
-import Image from 'next/image'
-import Link from "next/link"
-import { HiArrowDown } from "react-icons/hi"
-
+import { ThemeProvider } from 'next-themes'
+import HeroSection from "./components/herosection"
+import AboutSection from "./components/AboutSection"
 
 export default function Home() {
   return (
-    <main> 
+    <main>
       <ThemeProvider enableSystem={true} attribute="class">
-      <Navbar />
-      {/* create a hero suction */}
-      <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-        <div className="md:mt-2 md:w-1/2">
-          <Image
-            src="/headshot.png"
-            alt=""
-            width={325}
-            height={325}
-            className="rounded-full shadow-2xl"
-          />
+        <Navbar />
+        <div className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+          <HeroSection />
+          <AboutSection />
         </div>
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Abderrahim!</h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&#39;m a{" "}
-            <span className="font-semibold text-teal-600">
-              Software Engineer{" "}
-            </span>
-            based in Los Angeles, CA. Working towards creating software that
-            makes life easier and more meaningful.
-          </p>
-          {/* <Link
-            to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Projects
-          </Link> */}
-        </div>
-      </div>
-      <div className="flex flex-row items-center text-center justify-center ">
-        {/* <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        > */}
-          <HiArrowDown size={35} className="animate-bounce" />
-        {/* </Link> */}
-      </div>
-    </section>
       </ThemeProvider>
     </main>
   )
